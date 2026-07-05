a0be96e94376d4f6c02b70f877148dfdc25701ff
google_source <asssv0423348@gmail.com>
2026-07-05 16:38:03 +0300

    Update handle error in repository layer
    add domain error, checking for them, add op in errors returned

--- domain/errors.go
+++ domain/errors.go
@@ -1,5 +1,9 @@
 package domain
 
+import "errors"
+
 var (
-	
+	ErrEmail       = errors.New("such an email has already been registered")
+	ErrUserNoFound = errors.New("user is not found")
+	ErrPasswordOrEmail    = errors.New("invalid password or email")
 )
