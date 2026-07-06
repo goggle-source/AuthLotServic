9607ef91c04653ea06ef67a799b0556ba70ecad9
google_source <asssv0423348@gmail.com>
2026-07-06 14:19:07 +0300

    "m"

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -3,11 +3,11 @@ package repository
 import "errors"
 
 var (
-	ErrEnique   = errors.New("duplicate key")
-	ErrNotNull  = errors.New("the field is required")
-	ErrDatabase = errors.New("database error")
-	ErrMaxConn  = errors.New("the maximum number of connections is exceeded")
-	ErrNoRights = errors.New("no rights")
-	ErrPassword = errors.New("invalid password")
-	ErrLogin    = errors.New("invalid login")
+	ErrEnique      = errors.New("duplicate key")
+	ErrNotNull     = errors.New("the field is required")
+	ErrDatabase    = errors.New("database error")
+	ErrMaxConn     = errors.New("the maximum number of connections is exceeded")
+	ErrNoRights    = errors.New("no rights")
+	ErrPassword    = errors.New("invalid password")
+	ErrLogin       = errors.New("invalid login")
 )
